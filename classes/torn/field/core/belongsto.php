11d0d665f36130185ccd31273fdbf1dfd4b66462
TdroL <michal12.mail@gmail.com>
2010-08-18 18:20:11 +0200

    Added 'None' option to belongsto field

--- classes/torn/field/core/belongsto.php
+++ classes/torn/field/core/belongsto.php
@@ -5,10 +5,17 @@ class Torn_Field_Core_BelongsTo extends Torn_Field
 
 	public function input(array $attributes = array())
 	{
+		$options = Jelly::select($this->field->foreign['model'])->execute()->as_array(':primary_key', ':name_key');
+		
+		if($this->field->null)
+		{
+			$options = array_merge(array(0 => __('None')), $options);
+		}
+		
 		$this->view->set(array(
 			'name' => $this->field->name,
 			'value' => $this->model->__get($this->field->name),
-			'options' => Jelly::select($this->field->foreign['model'])->execute()->as_array(':primary_key', ':name_key'),
+			'options' => $options,
 			'attributes' => $attributes,
 		));
 		
