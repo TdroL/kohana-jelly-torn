9850ed5ded2469e3132b58d9ab9b4672caec26cc
TdroL <michal12.mail@gmail.com>
2010-08-20 14:56:42 +0200

    Still fixing #2

--- classes/torn/field/core/belongsto.php
+++ classes/torn/field/core/belongsto.php
@@ -7,14 +7,16 @@ class Torn_Field_Core_BelongsTo extends Torn_Field
 	{
 		$options = Jelly::select($this->field->foreign['model'])->execute()->as_array(':primary_key', ':name_key');
 		
-		if($this->field->null AND ! array_key_exists('not_empty', $this->field->rules))
+		$value = $this->model->__get($this->field->name);
+		
+		if($this->field->null OR empty($value) AND ! array_key_exists('not_empty', $this->field->rules))
 		{
 			$options = array_merge(array('' => __('None')), $options);
 		}
 		
 		$this->view->set(array(
 			'name' => $this->field->name,
-			'value' => $this->model->__get($this->field->name),
+			'value' => $value,
 			'options' => $options,
 			'attributes' => $attributes,
 		));
